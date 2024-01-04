3f213e7454eec86473647619bcf8f5e8de7e8098
Mateus Sales Albino <mateus3009@gmail.com>
2024-01-03 22:35:39 -0300

    TODO LIST

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,66 @@
 use yew::prelude::*;
 
 #[derive(Clone, PartialEq)]
-struct Video {
+struct Todo {
     id: usize,
     title: String,
-    speaker: String,
-    url: String,
+    description: String,
 }
 
-#[derive(Properties, PartialEq)]
-struct VideosListProps {
-    videos: Vec<Video>,
-    on_click: Callback<Video>,
-}
+#[function_component(TodoList)]
+fn todo_list() -> Html {
+    let todos = use_state(|| vec![Todo {
+        id: 1,
+        title: "Learn Rust".to_string(),
+        description: "Learn how to use Rust".to_string(),
+    }]);
 
-#[function_component(VideosList)]
-fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
-    videos
-        .iter()
-        .map(|video| {
-            let on_video_select = {
-                let on_click = on_click.clone();
-                let video = video.clone();
-                Callback::from(move |_| on_click.emit(video.clone()))
-            };
-            html! {
-                <p key={video.id} onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
-            }
-        })
-        .collect::<Html>()
-}
-
-#[derive(Properties, PartialEq)]
-struct VideosDetailsProps {
-    video: Video,
-}
-
-#[function_component(VideoDetails)]
-fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
     html! {
-        <div>
-            <h3>{ video.title.clone() }</h3>
-            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+        <div class="container">
+            <header>
+                <hgroup>
+                    <h1>{ "TODO LIST" }</h1>
+                    <h2>{ "Get it done today!" }</h2>
+                </hgroup>
+            </header>
+            <article>
+                <form>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        aria-label="Title"
+                        required={true}
+                    />
+                    <input
+                        type="text"
+                        name="description"
+                        placeholder="Description"
+                        aria-label="Description"
+                        required={true}
+                    />
+                    <button type="submit">{"Create"}</button>
+                </form>
+            </article>
+            <main class="grid">
+                {
+                    for todos.iter().map(|todo| {
+                        html! {
+                            <article>
+                                <header>{ &todo.title }</header>
+                                { &todo.description }
+                                <footer>
+                                    <button>{ "Done!" }</button>
+                                </footer>
+                            </article>
+                        }
+                    })
+                }
+            </main>
         </div>
     }
 }
 
-#[function_component(App)]
-fn app() -> Html {
-    let videos = vec![
-        Video {
-            id: 1,
-            title: "Building and breaking things".to_string(),
-            speaker: "John Doe".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 2,
-            title: "The development process".to_string(),
-            speaker: "Jane Smith".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 3,
-            title: "The Web 7.0".to_string(),
-            speaker: "Matt Miller".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 4,
-            title: "Mouseless development".to_string(),
-            speaker: "Tom Jerry".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-    ];
-
-    let selected_video = use_state(| | None);
-
-    let on_video_select = {
-        let selected_video = selected_video.clone();
-        Callback::from(move |video: Video| {
-            selected_video.set(Some(video));
-        })
-    };
-
-    let details = selected_video.as_ref().map(|video| {
-        html! {
-            <VideoDetails video={video.clone()} />
-        }
-    });
-
-    html! {
-        <>
-            <h1>{ "RustConf Explorer" }</h1>
-            <div>
-                <VideosList videos={videos} on_click={on_video_select.clone()} />
-            </div>
-            { for details }
-        </>
-    }
-}
-
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<TodoList>::new().render();
 }
